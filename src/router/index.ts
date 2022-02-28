9fe29b774777f6aadc1cc4934b175bbe2a4ecb35
weiwentao <17600626753@163.com>
2022-02-28 18:15:29 +0800

    test gitHooks

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
 
 Vue.use(VueRouter)
-
+console.log(first)
 const routes: Array<RouteConfig> = [
 
   // {
@@ -12,7 +12,8 @@ const routes: Array<RouteConfig> = [
   //   // this generates a separate chunk (about.[hash].js) for this route
   //   // which is lazy-loaded when the route is visited.
   //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // }
+  // }import { log } from '../../../../../02/02-02-study-materials/codes/02-02-03-01-rollup-demo/07-multi-entry/src/logger';
+
 ]
 
 const router = new VueRouter({
